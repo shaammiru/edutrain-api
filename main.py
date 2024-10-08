b87a07ffabded54e0910ad742f55b51b4e966804
Shaammiru <msyamil.hamami@gmail.com>
2024-10-08 17:27:18 +0700

    initial commit: setup and install fastapi

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read():
+    return {"hello": "world"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=3000)
